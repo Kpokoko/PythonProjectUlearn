6493dec8bd80df9ae72926ca0975eea71d66d695
Krokoko <142137027+Kpokoko@users.noreply.github.com>
2024-12-26 01:56:30 +0500

    commit try 17

---
+++ PYPROJ/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from analytics import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('hello/', views.index, name='index'),
+]
