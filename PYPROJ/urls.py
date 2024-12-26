61ce65527130c3bcceb472f389440a5e481cf23d
Krokoko <142137027+Kpokoko@users.noreply.github.com>
2024-12-26 22:50:28 +0500

    added currencies info from cb RF to bd, created salaries_by_year table algorithm

--- PYPROJ/urls.py
+++ PYPROJ/urls.py
@@ -6,4 +6,5 @@ from analytics import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', views.index, name='index'),
+    path('general_statistics/', views.general_statistics, name='general_statistics'),
 ]
