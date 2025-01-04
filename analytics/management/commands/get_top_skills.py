f23dbbe5e2e6d62197d9f70c71b5e45e998f2e93
Krokoko <142137027+Kpokoko@users.noreply.github.com>
2025-01-04 19:14:46 +0500

    fixed top_skills page yayy

--- analytics/management/commands/get_top_skills.py
+++ analytics/management/commands/get_top_skills.py
@@ -1,42 +1,32 @@
 from django.core.management.base import BaseCommand
-from django.db.models import Count
-from django.db.models.functions import ExtractYear
-from collections import defaultdict, Counter
 from analytics.models import Vacancy, Top_skills
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        # Удалим старые данные
         Top_skills.objects.all().delete()
-
-        # Получаем данные вакансий с ненулевым полем published_at
-        data = Vacancy.objects.filter(published_at__isnull=False)
-
-        # Аннотируем и группируем по годам
-        data = (data.annotate(year=ExtractYear('published_at'))
-                .values('year', 'key_skills')
-                .annotate(number_of_vacancies=Count('id')))
-
-        skills_by_year = defaultdict(Counter)
-
+        data = Vacancy.objects.all()
+        skills_by_year = {}
         for item in data:
-            year = item['year']
-            skills_str = item['key_skills']
+            year = item.published_at.year
+            skills_str = item.key_skills
 
             # Разбиваем строку навыков на отдельные навыки, разделенные запятыми и переносами строк
             if isinstance(skills_str, str):
-                skills_list = []
-                for separator in [',', '\n']:
-                    skills_list.extend(skill.strip() for skill in skills_str.split(separator) if skill.strip())
+                skills_list = skills_str.replace('\n', ',').replace(';', ',').split(',')
+                skills_list = [skill.strip() for skill in skills_list if skill.strip()]
 
                 for skill in skills_list:
+                    if year not in skills_by_year:
+                        skills_by_year[year] = {}
+                    if skill not in skills_by_year[year]:
+                        skills_by_year[year][skill] = 0
                     skills_by_year[year][skill] += 1
 
         # Оставляем топ-20 навыков для каждого года и сортируем
-        top_skills_by_year = {year: dict(counts.most_common(20)) for year, counts in skills_by_year.items()}
-        sorted_top_skills = sorted(top_skills_by_year.items())
+        sorted_top_skills = {year: dict(sorted(counts.items(), key=lambda item: item[1], reverse=True)[:20]) for
+                             year, counts in skills_by_year.items()}
 
         # Сохраняем в модель Top_skills
-        for year, skills_freq in sorted_top_skills:
-            Top_skills.objects.create(year=year, skills_list=skills_freq)
+        for year, skills in sorted_top_skills.items():
+            Top_skills.objects.create(year=year, skills_list=skills)
