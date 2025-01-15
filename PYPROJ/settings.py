dc26df0a121a95977e44001391eaa26431ecb8bd
Krokoko <142137027+Kpokoko@users.noreply.github.com>
2025-01-15 16:28:15 +0500

    last vacs falls i'll try to fix

--- PYPROJ/settings.py
+++ PYPROJ/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pkx=^90a%ev(vk*-%npzl##y$e^$p#)t_sqpziz*oi^s&!z7%d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
